bea741c66304c54614d0772ed0cf96612d52994f
UrTypicalMike <chukwumaudengwu05@gmail.com>
2025-07-04 15:11:42 +0100

    Update school-project.py

--- school-project.py
+++ school-project.py
@@ -1,15 +1,18 @@
-score = int(input("Enter your score: "))
-if 70 <= score <= 100:
-    print("A")
-elif 60 <= score < 70:
-    print("B")
-elif 50 <= score < 60:
-    print("C")
-elif 45 <= score < 50:
-    print("D")
-elif 40 <= score < 45:
-    print("E")
-elif 0 <= score < 40:
-    print("F")
-else:
-    print("Invalid score")
+try:
+    score = int(input("Enter the score: "))
+    if 70 <= score <= 100:
+        print("A")
+    elif 60 <= score < 70:
+        print("B")
+    elif 50 <= score < 60:
+        print("C")
+    elif 45 <= score < 50:
+        print("D")
+    elif 40 <= score < 45:
+        print("E")
+    elif 0 <= score < 40:
+        print("F")
+    else:
+        print("Invalid score")
+except ValueError:
+    print("Invalid input. Please enter a numeric score.")
